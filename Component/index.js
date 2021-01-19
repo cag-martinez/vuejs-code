c4c018b2b517c1061b3ba0633937eab7d0429e3b
Christian Gomez Martinez <gomez9289@gmail.com>
2021-01-18 22:44:38 -0500

    displaying my ordered list in the browser

--- Component/index.js
+++ Component/index.js
@@ -1,12 +1,15 @@
 Vue.component('todo-item', {
-    template: '<li>This is a to-do</li>'
+    props: ['todo'],
+    template: '<li>{{ todo.text}}</li>'
 })
 
 var app= new Vue({
-    el="#app7",
+    el:"#app7",
     data: {
         groceryList:[
-            {id:0, text:"lettuce"}
+            {id:0, text:"lettuce"},
+            {id:1, text:'tomatoes'},
+            {id:2, text:'avocado'}
         ]
     }
 })
