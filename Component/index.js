ef20f27fe52bba41cb41f5e09378a8de6091c21b
Christian Gomez Martinez <gomez9289@gmail.com>
2021-01-18 22:40:29 -0500

    directives

--- Component/index.js
+++ Component/index.js
@@ -1,13 +1,12 @@
-vue.component('todo-item', {
+Vue.component('todo-item', {
     template: '<li>This is a to-do</li>'
 })
 
 var app= new Vue({
-    el="app",
+    el="#app7",
     data: {
         groceryList:[
             {id:0, text:"lettuce"}
         ]
-
     }
 })
