5a45ea0f1888f31a7c80b0752acdde08a2d59055
Christian Gomez Martinez <gomez9289@gmail.com>
2021-02-05 16:30:29 -0500

    images with route links also basic style

--- Router/routes-practice/src/views/Home.vue
+++ Router/routes-practice/src/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="home">
+    <h1>All destinations</h1>
+    <div class="destinations">
     <!-- using a v-for directive to loop through all of m y destinations in store.js -->
     <!-- Every time i use the V-for directive, i have to use a :key attribute to know which element is which -->
     <div v-for="destination in destinations" :key="destination.name">
       <router-link :to="destination.slug">
         <h2>{{ destination.name }}</h2>
+      </router-link> 
+      <figure>
+      <router-link  :to="destination.name">
+        <img :src="require(`@/assets/${destination.image}`)" :alt="destination.name">
       </router-link>
-
+      </figure>
     </div>
+  </div>
   </div>
 </template>
 
@@ -25,3 +32,12 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.destinations {
+  display: flex;
+  border: 10px;
+  padding: 5px;
+  flex-wrap: wrap;
+}
+</style>
